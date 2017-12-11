2632c57d2eaa420312737a79c3908bad2bcc95d1
Paul GOERGLER <pgoergler@gmail.com>
2017-12-11 18:13:06 +0400

    UPGRADE: to pimple 3.x

--- src/Quartz/QuartzGuard/QuartzGuardServiceProvider.php
+++ src/Quartz/QuartzGuard/QuartzGuardServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace Quartz\QuartzGuard;
 
-use Silex\Application,
+use Pimple\Container,
     Symfony\Component\HttpFoundation\Session\Storage\Handler\NativeFileSessionHandler,
     Symfony\Component\HttpFoundation\Session\Storage\NativeSessionStorage,
     Symfony\Component\HttpFoundation\Request,
@@ -17,12 +17,12 @@ use Silex\Application,
 class QuartzGuardServiceProvider extends \Silex\Provider\SessionServiceProvider
 {
 
-    public function boot(Application $app)
+    public function boot(Container $app)
     {
         $app['orm']->init($app['quartz.databases']); // to init database
     }
 
-    public function register(Application $app)
+    public function register(Container $app)
     {
         $this->app = $app;
 
@@ -44,7 +44,7 @@ class QuartzGuardServiceProvider extends \Silex\Provider\SessionServiceProvider
 
         $app['session.test'] = false;
 
-        $app['session'] = $app->share(function ($app) {
+        $app['session'] = function ($app) {
             if (!isset($app['session.storage'])) {
                 if ($app['session.test']) {
                     $app['session.storage'] = $app['session.storage.test'];
@@ -54,22 +54,22 @@ class QuartzGuardServiceProvider extends \Silex\Provider\SessionServiceProvider
             }
 
             return new \Quartz\QuartzGuard\Session($app['orm'], $app['quartzguard.config.user'], $app['session.storage'], null, null, $app['quartzguard.config.prefix']);
-        });
+        };
 
-        $app['session.storage.handler'] = $app->share(function ($app) {
+        $app['session.storage.handler'] = function ($app) {
             return new NativeFileSessionHandler($app['session.storage.save_path']);
-        });
+        };
 
-        $app['session.storage.native'] = $app->share(function ($app) {
+        $app['session.storage.native'] = function ($app) {
             return new NativeSessionStorage(
                 $app['session.storage.options'],
                 $app['session.storage.handler']
             );
-        });
+        };
 
-        $app['session.storage.test'] = $app->share(function() {
+        $app['session.storage.test'] = function() {
             return new MockFileSessionStorage();
-        });
+        };
 
         $app['guard.must_be_authenticated'] = $app->protect(function(\Symfony\Component\HttpFoundation\Request $request) use (&$app)
                 {
@@ -152,5 +152,3 @@ class QuartzGuardServiceProvider extends \Silex\Provider\SessionServiceProvider
     }
 
 }
-
-?>
